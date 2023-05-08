a38e8a41093ab0e932ec72704f2cedce6b6a956e
ANDRE BAPTISTA <andrebaptista@petrobras.com.br>
2023-05-08 12:16:18 -0300

    v.0.0.4

---
+++ System/Main.s
@@ -0,0 +1,52 @@
+FNAME "msx-windows.rom"      ; output file
+
+PageSize:	    equ	0x4000	        ; 16kB
+
+; Compilation address
+    org 0x4000
+
+    INCLUDE "../Include/RomHeader.s"
+    INCLUDE "../Include/MsxBios.s"
+    INCLUDE "../Include/MsxConstants.s"
+    INCLUDE "../Include/CommonRoutines.s"
+
+; Assets
+    INCLUDE "../Fonts/Font_Normal.s"
+
+
+; Apps
+    INCLUDE "../Apps/Notepad/Header.s"
+    ;INCLUDE "../Apps/Calc/Header.s"
+
+
+
+Execute:
+    ; define screen colors
+    ld 		a, 15      	            ; Foreground color
+    ld 		(BIOS_FORCLR), a    
+    ld 		a, 1  		            ; Background color
+    ld 		(BIOS_BAKCLR), a     
+    ld 		a, 1      	            ; Border color
+    ld 		(BIOS_BDRCLR), a    
+    call 	BIOS_CHGCLR        		; Change Screen Color
+
+
+
+
+    ; code here
+
+
+
+
+
+
+    db      "End ROM started at 0x4000"
+
+	ds PageSize - ($ - 0x4000), 255	; Fill the unused area with 0xff
+
+
+
+; RAM
+	org     0xc000, 0xe5ff
+
+    ;INCLUDE "Ram.s"
---
+++ System/Ram.s
@@ -0,0 +1,60 @@
+OS:
+
+.mouseY:		        rb 1
+.mouseX:		        rb 1
+.mousePattern:	        rb 1
+.mouseColor:	        rb 1
+.mouseY_1:		        rb 1
+.mouseX_1:		        rb 1
+.mousePattern_1:	    rb 1
+.mouseColor_1:	        rb 1
+
+.keyboardMatrix:	    rb 10			; https://map.grauw.nl/articles/keymatrix.php
+
+.ticksSinceLastInput:	rw 1		    ; used to trigger screen saver
+
+.currentTime:	        rb 3			; BCD encoded time hh:mm:ss
+.currentDate:	        rb 3			; BCD encoded date dd/mm/yyyy
+.timeCounter	        rb 1			; 0-59 JIFFY based counter to increment time/date (0-49 on PAL machines)
+
+; TODO: this can be moved to the free VRAM area (addr 7040)
+.screenMapping: 	    rb 32*24		; used to map each tile to a window/desktop (useful on mouse click/over). 
+                                        ; 255: desktop, 254: bottom bar;
+                                        ; 4 high bits: window title buttons/resize corner, 4 lower bits (0-15): process id
+
+.currentProcessAddr:    rw 1
+
+.processes:		        rb Process_struct.size * 4 ; maybe 6 in the future
+
+
+Process_struct:
+.processId:		        rb 1		    ; 255: empty
+.windowState:	        rb 1		    ; 0: minimized, 1: restored, 2: maximized
+.x:			            rb 1
+.y:			            rb 1
+.width:		            rb 1
+.height:		        rb 1
+.minWidth:		        rb 1
+.minHeight:		        rb 1
+
+.windowTitle:	        rb 16
+.isFixedSize:	        rb 1
+.vertScrollbarEnabled:	rb 1
+.vertScrollbarPosition:	rb 1
+
+
+.openAddr:		        rw 1
+.workAddr:		        rw 1
+.drawAddr:		        rw 1
+.clickAddr:		        rw 1
+.closeAddr:		        rw 1
+
+.ramStartAddr:	        rw 1
+.ramSize:		        rw 1
+
+.vramStartTileAddr:	    rw 1
+
+.screenTilesBehind:	    rb 32*24
+
+.size: equ $ - Process_struct
+
