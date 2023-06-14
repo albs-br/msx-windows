095a0fe6e5737804eefa54eff9b91b27dc294c4d
ANDRE BAPTISTA <andrebaptista@petrobras.com.br>
2023-06-14 12:08:08 -0300

    v.0.46.1

--- System/Keyboard/Keyboard.s
+++ System/Keyboard/Keyboard.s
@@ -1,75 +1,69 @@
 	; OLDKEY:	equ $fbda ; Previous state of the keyboard matrix (11 bytes)
 	; NEWKEY:	equ $fbe5 ; Current state of the keyboard matrix (11 bytes)
-	;	 OLDKEY  NEWKEY
-	;		|      |
-	; 	; $fbda, $fbe5 ; 7 6 5 4 3 2 1 0
-	; 	; $fbdb, $fbe6 ; ; ] [ \ = - 9 8
-	; 	; $fbdc, $fbe7 ; B A pound / . , ` '
-	; 	; $fbdd, $fbe8 ; J I H G F E D C
-	; 	; $fbde, $fbe9 ; R Q P O N M L K
-	; 	; $fbdf, $fbea ; Z Y X W V U T S
-	; 	; $fbe0, $fbeb ; F3 F2 F1 CODE CAP GRAPH CTRL SHIFT
-	; 	; $fbe1, $fbec ; CR SEL BS STOP TAB ESC F5 F4
-	; 	; $fbe2, $fbed ; RIGHT DOWN UP LEFT DEL INS HOME SPACE
-	; 	; $fbe3, $fbee ; 4 3 2 1 0 none none none
-	; 	; $fbe4, $fbef ; . , - 9 8 7 6 5
+	;   	     OLDKEY  NEWKEY
+	;   		    |      |
+	; line 0	; $fbda, $fbe5 ; 7 6 5 4 3 2 1 0
+	; line 1	; $fbdb, $fbe6 ; ; ] [ \ = - 9 8
+	; line 2	; $fbdc, $fbe7 ; B A pound / . , ` '
+	; line 3	; $fbdd, $fbe8 ; J I H G F E D C
+	; line 4	; $fbde, $fbe9 ; R Q P O N M L K
+	; line 5	; $fbdf, $fbea ; Z Y X W V U T S
+	; line 6	; $fbe0, $fbeb ; F3 F2 F1 CODE CAP GRAPH CTRL SHIFT
+	; line 7	; $fbe1, $fbec ; CR SEL BS STOP TAB ESC F5 F4
+	; line 8	; $fbe2, $fbed ; RIGHT DOWN UP LEFT DEL INS HOME SPACE
+	; line 9	; $fbe3, $fbee ; 4 3 2 1 0 none none none
+	; line 10	; $fbe4, $fbef ; . , - 9 8 7 6 5
 
 
 _READ_KEYBOARD:
 
-    ld      a, (BIOS_NEWKEY + 8)
-    ld      b, a
+    ; check if GRAPH (ALT) key is pressed
+    ld      a, (BIOS_NEWKEY + 6)
+    bit     2, a
+    ret     nz
 
-    push    bc
-        bit     7, b ; right arrow
-        call    z, .keyPressed_R
-    pop     bc
+    ; L = BIOS_NEWKEY + 3, H = BIOS_NEWKEY + 4
+    ld      hl, (BIOS_NEWKEY + 3)
 
-    push    bc
-        bit     4, b ; left arrow
-        call    z, .keyPressed_L
-    pop     bc
+    push    hl
+        bit     3, h ; 'N' key
+        jp      z, .keyPressed_N
+
+        bit     0, l ; 'C' key
+        jp      z, .keyPressed_C
+
+        .continue:
+    pop     hl
 
 
     ; update old keyboard state
-    ld      a, b
-    ld      (OS.oldKeyboardMatrix + 8), a
-
-    ; ld      hl, (BIOS_NEWKEY + 0)
-    ; ld      (OS.keyboardMatrix + 0), hl
+    ld      (OS.oldKeyboardMatrix + 3), hl
 
     ret
 
-.keyPressed_R:
+.keyPressed_N:
     ; check if key was previously released
-    ld      a, (OS.oldKeyboardMatrix + 8)
-    bit     7, a
-    ret     z
+    ld      a, (OS.oldKeyboardMatrix + 4)
+    bit     3, a ; 'N' key
+    jp      z, .continue
 
     ; execute key pressed code here
-    ; call    BIOS_BEEP
     ld      hl, Notepad.Header
     call    _LOAD_PROCESS
 
-    ret
+    jp      .continue
 
-.keyPressed_L:
+.keyPressed_C:
     ; check if key was previously released
-    ld      a, (OS.oldKeyboardMatrix + 8)
-    bit     4, a
-    ret     z
+    ld      a, (OS.oldKeyboardMatrix + 3)
+    bit     0, a ; 'C' key
+    jp      z, .continue
 
     ; execute key pressed code here
-    ; ld      b, 20
-    ; .test:
-    ;     push    bc
-    ;         call    BIOS_BEEP
-    ;     pop bc
-    ; djnz  .test
     ld      hl, Calc.Header
     call    _LOAD_PROCESS
 
-    ret
+    jp      .continue
 
 
 ; _READ_KEYBOARD_____: 
@@ -95,3 +89,10 @@ _READ_KEYBOARD:
 ;     and	b			   ; old value
 ;     xor	c			                        
 ;     ret
+
+
+; Returns ASCII code of keypressed (only positive trnsition)
+; Output:
+;   A: ASCII code of key
+call    READ_KEYBOARD
+    ret
